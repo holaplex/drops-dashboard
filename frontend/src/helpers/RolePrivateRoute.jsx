5116e9b87fadf8cd6f1f4d3189cc9eb773028f35
Pedro Lucas <pedro.leoti.pessoal@gmail.com>
2022-05-16 14:55:54 -0300

    Fixing check for user type

--- frontend/src/helpers/RolePrivateRoute.jsx
+++ frontend/src/helpers/RolePrivateRoute.jsx
@@ -1,16 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import * as userApi from '../features/User/userApi';
+
 import { userSelector } from '../features/User/userSlice';
+import { getToken } from '../helpers/localStorage';
+import { fetchUserBytoken } from '../features/User/userActions';
+import store from '../app/store';
 
 const RolePrivateRoute = ({ children, roles = [] }) => {
-    const user = useSelector(userSelector)
-    const authorized = roles.includes(user.user_type);
+    const [authorized, setAuthorizded] = useState()
+    const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        async function getUserData() {
+            const token = getToken().access_token;
+
+            if (token) {
+                const user = await store.dispatch(fetchUserBytoken())
+                const authorized = roles.includes(user.payload.user_type);
+                setAuthorizded(authorized)
+            }
+            setLoading(false)
+        }
+        getUserData()
+    }, [])
     // If authorized, return an outlet that will render child elements
     // If not, return element that will navigate to login page
-    return authorized ? children : <Navigate to='/drops' />;
+
+    return loading ? (<h1>loading</h1>) : (authorized ? children : <Navigate to='/drops' />);
 };
 
 export default RolePrivateRoute;
