140b71ee68add4e806cbce4fb22487baf0eb5110
plucks77 <pedro.leoti.pessoal@gmail.com>
2022-06-27 16:57:36 -0300

    Removing new_key, adding script to run project locally and updating .env.example

--- backend/app/controllers/nft_controller.rb
+++ backend/app/controllers/nft_controller.rb
@@ -39,7 +39,7 @@ class NftController < ApplicationController
           # path = nft.make_watermark("./public/images#{nft[:final_filename]}",nft_final_media, nft_drop.name)
           # Net::SCP.upload!("assets.campuslegends.com", "assets",
           #   path, "/home/assets/assets/images/preview-videos", 
-          #   :ssh => { :keys => "new_key", :passphrase => 'new_key' })
+          #   :ssh => { :keys => "key"})
           # nft[:preview_url] = "https://assets.campuslegends.com/images/preview-videos/#{nft_final_media[:name]}"
 
           nft[:nft_drop_id] = nft_drop[:id]
